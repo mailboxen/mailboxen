9dc7e6508ccc3466e511fba6d4585bd968f1f53e
dhfromkorea <dhllee424@gmail.com>
2014-08-03 13:09:28 -0700

    fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var app = require('./server/server');
-var port = process.env.port || 8000;
+var port = process.env.PORT || 8000;
 app.listen(port);
 console.log('the server is listening to ' + port);
