b595094a66c2a61e0ee2dc57e6f5f054b9214cf7
dhfromkorea <dhllee424@gmail.com>
2014-08-03 12:51:15 -0700

    yaml generate ready

--- server/controller.js
+++ server/controller.js
@@ -2,14 +2,15 @@ var promise = require('bluebird');
 var fs = promise.promisifyAll(require("fs"));
 var xmlParser = require('xml2js');
 var Ansible = require('node-ansible');
+var yaml = require('yamljs');
 
-var configPath = 'data/config.txt';
+var configPath = 'data/config.yml';
 var xmlPath = 'data/log.xml';
 
 
 var parseConfig = function(obj) {
   var result = [];
-  var list = ["DOMAIN", "DNS", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"];
+  var list = ["domain", "dns", "aws_access_key", "aws_secret_key"];
   for (var key in obj) {
     if (list.indexOf(key) > -1) {
       result.push(key + "=" + obj[key]);
@@ -21,7 +22,7 @@ var parseConfig = function(obj) {
 var reverseParseConfig = function(string) {
   var arr = string.split('\n');
   var obj = {};
-  var list = ["DOMAIN", "DNS", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"];
+  var list = ["domain", "dns", "aws_access_key", "aws_secret_key"];
   arr.forEach(function(line) {
     line = line.split('=');
     obj[line[0]] = line[1];
@@ -30,6 +31,12 @@ var reverseParseConfig = function(string) {
   return obj;
 };
 
+var buildYaml = function(obj){
+  // var obj = JSON.stringify(obj);
+  obj.aws_region = "us-west-2";
+  obj.users = [{ name: "user1", password: 'sdfdsfsff' },{ name: "user2", password: 'sdfsdfdf' }];
+  return yaml.stringify(obj, 2);
+};
 
 var executeAnsible = function() {
   var command = new Ansible.AdHoc().module('shell').hosts('local').args("echo 'hello'");
@@ -40,8 +47,9 @@ var parseString = promise.promisify(new xmlParser.Parser().parseString);
 var executeAnsible = promise.promisify(executeAnsible);
 
 module.exports.writeFile = function(req, res) {
-  var data = parseConfig(req.body);
+  // var data = parseConfig(req.body);
   // filepath === relative to the roote directory
+  var data = buildYaml(req.body);
   fs.writeFileAsync(configPath, data)
     .then(function(fullPath) {
       // executeAnsible();
